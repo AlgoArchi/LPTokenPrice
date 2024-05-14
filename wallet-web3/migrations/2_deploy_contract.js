cdfc243f1c43f69d02dede2eb30a32fd03d5954f
faiezharis1230 <faiezharis1230@gmail.com>
2024-05-14 17:21:55 +0900

    get LP token price

--- wallet-web3/migrations/2_deploy_contract.js
+++
@@ -1,9 +0,0 @@
-const BoomOutCoin = artifacts.require("BoomOutCoin");
-const Airdrop = artifacts.require("Airdrop");
-
-module.exports = async function(deployer) {
-    await deployer.deploy(BoomOutCoin);
-    const deployedBOC = await BoomOutCoin.deployed();
-    const BOCAddress = deployedBOC.address;
-    await deployer.deploy(Airdrop, BOCAddress);
-};
